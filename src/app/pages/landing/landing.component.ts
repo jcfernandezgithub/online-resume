c778822c6f8ef6d8260d80f05260acbdb5d59fc7
Jorge Cristian Fernandez <jcfernandez@outlook.com.ar>
2019-09-04 00:15:23 -0300

    sidebar_added

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -17,4 +17,18 @@ export class LandingComponent implements OnInit {
     return this.toastr.info('Copied!');
   }
 
+  /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+  openNav() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("openbtn").style.display = "none";
+  }
+
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  closeNav() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("openbtn").style.display = "block";
+  }
+
 }
